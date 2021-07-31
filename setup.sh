cf393e404cc03064454a482b35ce865417bbc9c9
Akshay Shirsath <40054782+akshayonly@users.noreply.github.com>
2021-08-01 01:49:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[theme]
-primaryColor = "#fdbf00"
-backgroundColor = "#ffffff"
-secondaryBackgroundColor = "#ededed"
-textColor= "#2e2e2e"
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+[theme]\n\
+primaryColor = "#fdbf00"\n\
+backgroundColor = "#ffffff"\n\
+secondaryBackgroundColor = "#ededed"\n\
+textColor= "#2e2e2e"\n\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
 " > ~/.streamlit/config.toml
