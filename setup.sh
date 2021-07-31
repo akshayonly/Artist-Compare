100673f3dd8b4aa191f025e24ec742de4d0a7506
Akshay Shirsath <40054782+akshayonly@users.noreply.github.com>
2021-08-01 01:30:13 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,5 @@ echo "\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
-[theme]
-primaryColor="#FDBF00"\n\
-backgroundColor="#ffffff"\n\
-secondaryBackgroundColor="#f1f2f6"\n\
-textColor="#2f3542"\n\
 \n\
 " > ~/.streamlit/config.toml
